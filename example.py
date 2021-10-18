6db66003aff360ca9f8cdc6feb2636e372145a49
Christopher Hampson <christopher.hampson.1989@gmail.com>
2021-10-18 11:04:03 +0100

    fixed a few bugs

--- example.py
+++ example.py
@@ -3,12 +3,13 @@ import random
 
 
 def sum_options(x,y):
+	## simple example
 
 	options = []
-	options.append(f"\item* {x+y}")							# correct option
-	options.append(f"\item {x*y}") 							# 'plausible' distractor
-	options.append(f"\item {x**y}")							# 'plausible' distractor
-	options.append(f"\item {random.choice(range(100))}")	# distractor
+	options.append(f"\item* {(x+y)}")							# correct option
+	options.append(f"\item {(x*y)}") 							# 'plausible' distractor
+	options.append(f"\item {(x-y)}")							# 'plausible' distractor
+	options.append(f"\item {(random.choice(range(100)))}")		# distractor
 
 	random.shuffle(options)		# not strictly necessary if moodle is set to shuffle options
 
@@ -18,6 +19,7 @@ def sum_options(x,y):
 
 if __name__=="__main__":
 
+	## load the LaTeX template 'example.tex'
 	Q = moodlequiz.Quiz("example.tex")
 
 	# add 10 variants
@@ -28,7 +30,8 @@ if __name__=="__main__":
 		y = random.choice(range(1,10))
 		options = sum_options(x,y)
 
-		## define new question data
+		## define new question data to be replaced
+		## this will substitute commands \x, \y, and \options in the TeX
 		data = {}
 		data['x'] = x
 		data['y'] = y
@@ -37,6 +40,12 @@ if __name__=="__main__":
 		## add new question to question set
 		Q.add_variant(data)
 
+		## alternatively 'add_variant' accepts a list of keyword arguments
+		#Q.add_variant(x=x,y=y,options='\n'.join(options))
+
+
+	## generate new LaTeX file output populated with question variants
+	## 'compile' will also compile the output with pdfLaTeX, which will also generate an XML file.
 	Q.export("output.tex",compile=True)
 
 
--- moodlequiz.py
+++ moodlequiz.py
@@ -22,7 +22,15 @@ class Quiz:
 				self.template.find("quiz").remove(x)
 
 
-	def add_variant(self,params):
+	def add_variant(self,*args,**kwargs):
+		if len(args)>1:
+			raise Exception("add_variant accepts either a dictionary of key-value pairs or a list of keyword arguments")
+
+		params = kwargs
+		if len(args)==1 and type(args[0])==dict:
+			params = {**args[0], **params}
+
+
 		X = TexSoup.TexSoup(str(self.__questions.expr))
 		for key in params:
 			X.find(key).replace_with(params[key])
@@ -30,6 +38,8 @@ class Quiz:
 
 
 	def export(self,filename='output.tex',compile=False):
+		if filename==self.__template:
+			raise Exception("Cannot overwrite template file. Choose a different name for the output.")
 		self.__texfile = os.path.abspath(filename).split(".")
 
 		self.template.find('quiz').insert(0,*self.__variants)
@@ -40,15 +50,3 @@ class Quiz:
 			os.system("mkdir build")
 			os.system(f"pdflatex --output-dir=build -synctex=0 -interaction=nonstopmode {filename}")
 
-
-
-
-
-Q = Quiz("scc/scc_quiz_template.tex")
-# Q.set_template("scc/scc_quiz_template.tex")
-question_set = [{'first':1,'last':2,'second':'33','third':'7656','diagram':''}]
-Q.add_variant(question_set[0])
-Q.add_variant(question_set[0])
-Q.add_variant(question_set[0])
-Q.add_variant(question_set[0])
-Q.export("aug-2021_test.tex",compile=True)
