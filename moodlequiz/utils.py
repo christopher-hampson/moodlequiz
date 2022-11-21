46d14e2dcbc808238c0f92a0b346ae9500657c30
Christopher Hampson <christopher.hampson.1989@gmail.com>
2022-11-21 10:22:42 +0000

    refactored CDATA object as subclass of string

--- moodlequiz/utils.py
+++ moodlequiz/utils.py
@@ -1,9 +1,15 @@
 
 
 
-class CDATA:
+class CDATA(str):
     def __init__(self,text):
-        self.text = text
+        self.__text__ = text
 
     def __repr__(self):
-        return f"<![CDATA[{self.text}]]>"
+        return f"CDATA({self.__text__})"
+
+    def __str__(self):
+        return f"<![CDATA[{self.__text__}]]>"
+
+    def __len__(self):
+        return len(self.__text__)
